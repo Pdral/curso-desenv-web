814268dabadda191eabfaae933504797ec61e5d4
Pdral <paulodralbuquerque@gmail.com>
2024-09-17 22:07:09 -0300

    Segmentação em back e frontend

---
+++ back.js
@@ -0,0 +1,161 @@
+const http = require('http');
+const fs = require('fs');
+const port = 8084;
+const jogos_path = "data/jogos/dados.json";
+const posts_path = "data/posts/dados.json";
+const users_path = "data/users/dados.json";
+const ids_path = "data/ids.json";
+
+http.createServer((req, res) => {
+	const u = new URL(req.url, `http://${req.headers.host}`);
+
+    switch (u.pathname) {
+
+        case '/usuarios':
+			handleGeneralFiles(req, res, u, users_path, "usuarios");
+			break;
+		case '/jogos':
+			handleGeneralFiles(req, res, u, jogos_path, "jogos");
+			break;
+		case '/posts':
+			handleGeneralFiles(req, res, u, posts_path, "posts");
+			break;
+		case '/cartas':
+			getCartasOfUser(res, u);
+			break;
+		case '/carta':
+			handleGetCarta(res, u);
+			break;
+    }
+
+}).listen(port, () => console.log(`Api is running on port ${port}`));
+
+function handleGeneralFiles(req, res, u, fileName, entity){
+	const cors = {'Access-Control-Allow-Origin': '*'};
+	params = u.searchParams;
+	const id = params.get("id");
+	switch(req.method) {
+		case 'GET':
+			let response
+			if(id == undefined){
+				response = list(fileName, entity);
+			} else {
+				response = find(fileName, entity, id);
+			}
+			res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' , 'Access-Control-Allow-Origin': '*'});
+			res.write(JSON.stringify(response, null, 2));
+			return res.end(); break;
+		case 'POST':
+			var body = '';
+			req.on('data', function (data) {
+				body += data;
+				body = JSON.parse(body);
+				var user = body;
+				save(fileName, entity, user);
+			});
+			req.on('end', function () {
+				res.writeHead(201, cors);
+				return res.end();
+			}); break;
+		case 'PUT':
+			del(fileName, entity, id);
+			var body = '';
+			req.on('data', function (data) {
+				body += data;
+				body = JSON.parse(body);
+				var user = body;
+				console.log(user);
+				save(fileName, entity, user);
+			}); 
+			req.on('end', function () {
+				res.writeHead(201, cors);
+				return res.end();
+			}); break;
+		case 'DELETE':
+			del(fileName, entity, id);
+			res.writeHead(204, cors);
+			return res.end(); break;
+	}; 
+}
+
+function nextId(entity){
+	var data = fs.readFileSync(ids_path , "utf8");
+	var ids = JSON.parse(data);
+	var id = ids[entity];
+	ids[entity] = Number(id) + 1;
+	fs.writeFileSync(ids_path, JSON.stringify(ids, null, 2), 'utf8');
+	return id;
+}
+
+function list(fileName, entity){
+	var file = fs.readFileSync(fileName , "utf8");
+	var data = JSON.parse(file)[entity];
+	if(entity == 'posts'){
+		for (let index = 0; index < data.length; index++) {
+			data[index]["user"] = find(users_path, "usuarios",data[index]["user"]);
+		}
+	}
+	return data;
+}
+
+function find(fileName, entity, id){
+	var data = list(fileName, entity);
+	return data.filter(o => Number(o["id"]) === Number(id))[0];
+}
+
+function save(fileName, entity, data){
+	if(data.id == undefined){
+		data.id = nextId(entity);
+	}
+	var allData = list(fileName, entity);
+	allData.push(data);
+	fs.writeFileSync(fileName, JSON.stringify({[entity]: allData}, null, 2), 'utf8');
+}
+
+function del(fileName, entity, id){
+	var data = list(fileName, entity);
+	const newData = data.filter(o => Number(o["id"]) !== Number(id));
+	fs.writeFileSync(fileName, JSON.stringify({[entity]: newData}, null, 2), 'utf8');
+}
+
+function getCartasOfUser(res, u){
+	params = u.searchParams;
+	const id = params.get("id");
+	var jogos = list(jogos_path, "jogos");
+	var cartas = [];
+	for (let i = 0; i < jogos.length; i++) {
+		var jogo = jogos[i];
+		for (let j = 0; j < jogo["cartas"].length; j++) {
+			var carta = jogo["cartas"][j];
+			if(carta["vendedor"] == id){
+				cartas.push(carta);
+			}
+		}
+	}
+	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' , 'Access-Control-Allow-Origin': '*'});
+	res.write(JSON.stringify(cartas, null, 2));
+	return res.end();
+}
+
+function handleGetCarta(res, u){
+	params = u.searchParams;
+	const id = params.get("id");
+	const carta = getCarta(id);
+	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' , 'Access-Control-Allow-Origin': '*'});
+	res.write(JSON.stringify(carta, null, 2));
+	return res.end();
+}
+
+function getCarta(id){
+	var jogos = list(jogos_path, "jogos");
+	for (let i = 0; i < jogos.length; i++) {
+		var jogo = jogos[i];
+		for (let j = 0; j < jogo["cartas"].length; j++) {
+			var carta = jogo["cartas"][j];
+			if(carta["id"] == id){
+				carta["vendedor"] = find(users_path, "usuarios", carta["vendedor"]);
+				return carta;
+			}
+		}
+	}
+}
---
+++ front.js
@@ -0,0 +1,344 @@
+const fs = require('fs');
+const path = require('path');
+const api = "http://localhost:8084";
+const port = 8083;
+const express = require('express');
+const app = express() 
+const jogos_dir = "data/jogos/dados.json";
+const posts_dir = "data/posts/dados.json";
+const users_dir = "data/users/dados.json";
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+const expressLayouts = require('express-ejs-layouts'); 
+app.use(expressLayouts) 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    const userId = req.query.user;
+
+	if(userId == undefined || userId == ""){
+		req.query.user = {"perfil": "visitante", "id": undefined};
+		next();
+	} else{
+		fetch(api + '/usuarios?id=' + userId).then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			var a = response.json();
+			return a;
+		})
+		.then(user => {
+			req.query.user = user;
+			next();
+		})
+	}
+});
+
+app.get('/', (req, res) => {
+	const user = req.query.user;
+	fetch(api + '/jogos').then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		var a = response.json();
+		return a;
+	})
+	.then(jogos => {
+		// Lê o cookie 'theme' enviado pelo cliente
+		const theme = req.cookies.theme || 'light'; 
+
+		// Define o CSS com base no tema
+		const selectedCSS = theme === 'dark' ? '/css/produtos2.css' : '/css/produtos.css';
+	
+		res.render('index', {jogos: jogos, header: setHeader(user), navclass: {"produtos": "active"}, "user": user, css: selectedCSS});
+	})
+})
+
+app.get('/cadastro', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/style2.css' : '/css/style.css';
+	res.render('cadastro', {layout: "no-header", "user": user, css: selectedCSS});
+})
+
+app.get('/login', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/style2.css' : '/css/style.css';
+	res.render('login', {layout: "no-header", "user": user, css: selectedCSS});
+})
+
+app.get('/esquecer-senha', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/style2.css' : '/css/style.css';
+	res.render('esquecer-senha', {layout: "no-header", "user": user, css: selectedCSS});
+})
+
+app.get('/comunidade', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/comunidade2.css' : '/css/comunidade.css';
+
+	fetch(api + '/jogos').then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		var a = response.json();
+		return a;
+	})
+	.then(jogos => {
+		fetch(api + '/posts').then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			var a = response.json();
+			return a;
+		})
+		.then(posts => {
+			res.render('comunidade', {jogos: jogos, header: setHeader(user), navclass: {"comunidade": "active"}, "user": user, posts: posts, css: selectedCSS});
+		})
+	})
+})
+
+app.get('/post/:id', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/post2.css' : '/css/post.css';
+	fetch(api + '/posts?id='+req.params.id).then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		var a = response.json();
+		return a;
+	})
+	.then(post => {
+		res.render('post', {header: setHeader(user), navclass: {"comunidade": "active"}, "user": user, post: post, css: selectedCSS});
+	})
+})
+
+app.get('/meus-produtos', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/produtos2.css' : '/css/produtos.css';
+
+	if (req.xhr) {
+		fetch(api + '/cartas?id=' + user["id"]).then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			var a = response.json();
+			return a;
+		})
+		.then(cartas => {
+			res.json({ 
+				cartasCompradas: user["cartas-compradas"],
+				cartasVendidas: user["cartas-vendidas"],
+				cartas: cartas || [] 
+			});
+		})
+		
+	} else {
+		res.render('meus-produtos', {header: setHeader(user), navclass: {"meus-produtos": "active"}, "user": user, dono: user, css: selectedCSS});
+	}})
+
+app.get('/meus-produtos/:id', (req, res) => {
+    const user = req.query.user;
+    const donoId = req.params.id;
+    const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/produtos2.css' : '/css/produtos.css';
+    
+    fetch(api + '/usuarios?id=' + donoId).then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		var a = response.json();
+		return a;
+	})
+	.then(dono => {
+		if (req.xhr) {
+			fetch(api + '/cartas?id=' + donoId).then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				var a = response.json();
+				return a;
+			})
+			.then(cartas => {
+				res.json({ 
+					cartasCompradas: dono["cartas-compradas"],
+					cartasVendidas: dono["cartas-vendidas"],
+					cartas: cartas || [] 
+				});
+			})
+			
+		} else {
+			res.render('meus-produtos', {
+				header: setHeader(user), 
+				navclass: { "meus-produtos": "active" }, 
+				user: user, 
+				dono: dono, 
+				css: selectedCSS
+			});
+		}})
+	});
+
+app.get('/editar-produto/:id', (req, res) => {
+	const user = req.query.user;
+	const cartaId = req.params.id;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/produtos2.css' : '/css/produtos.css';
+	
+	fetch(api + '/carta?id=' + cartaId).then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		var a = response.json();
+		return a;
+	})
+	.then(carta => {
+		res.render('editar-produto', {header: setHeader(user), navclass: {"meus-produtos": "active"}, "user": user, carta: carta, css: selectedCSS});
+	})
+})
+
+app.get('/produto/:id', (req, res) => {
+	const user = req.query.user;
+	const cartaId = req.params.id;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/produtos2.css' : '/css/produtos.css';
+	
+	fetch(api + '/carta?id=' + cartaId).then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		var a = response.json();
+		return a;
+	})
+	.then(carta => {
+		res.render('produto', {header: setHeader(user), navclass: {"produtos": "active"}, "user": user, carta: carta, css: selectedCSS});
+	})
+})
+
+app.get('/criar-post', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/post2.css' : '/css/post.css';
+	
+	fetch(api + '/jogos').then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		var a = response.json();
+		return a;
+	})
+	.then(jogos => {
+		res.render('criar-post', {jogos: jogos, header: setHeader(user), navclass: {"comunidade": "active"}, "user": user, css: selectedCSS});
+	})
+})
+
+app.get('/add-produto', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/produtos2.css' : '/css/produtos.css';
+
+	res.render('add-produto', {header: setHeader(user), navclass: {"meus-produtos": "active"}, "user": user, css: selectedCSS});
+})
+
+app.get('/adm', (req, res) => {
+	const user = req.query.user;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/adm2.css' : '/css/adm.css';
+
+	if (req.xhr) {
+		fetch(api + '/jogos').then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			var a = response.json();
+			return a;
+		})
+		.then(jogos => {
+			fetch(api + '/usuarios').then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				var a = response.json();
+				return a;
+			})
+			.then(usuarios => {
+				res.json({ 
+					jogos: jogos,
+					usuarios: usuarios
+				});
+			})
+		})
+	} else{
+		res.render('adm', {header: setHeader(user), navclass: {"adm": "active"}, "user": user, css: selectedCSS});
+	}
+	
+})
+
+app.get('/editar-usuario/:id', (req, res) => {
+	const user = req.query.user;
+	const userId = req.params.id;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/adm2.css' : '/css/adm.css';
+
+	fetch(api + '/usuarios?id=' + userId).then(response => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		var a = response.json();
+		return a;
+	})
+	.then(usuario => {
+		res.render('editar-usuario', {usuario: usuario, header: setHeader(user), navclass: {"adm": "active"}, "user": user, css: selectedCSS});
+	})
+})
+
+app.get('/editar-jogo/:id', (req, res) => {
+	const user = req.query.user;
+	const jogoId = req.params.id;
+	let jogo;
+	const theme = req.cookies.theme || 'light'; 
+    const selectedCSS = theme === 'dark' ? '/css/adm2.css' : '/css/adm.css';
+
+	if(jogoId == 'new'){
+		jogo = {"nome": 'Nome do jogo'};
+		res.render('editar-jogo', {jogo: jogo, header: setHeader(user), navclass: {"adm": "active"}, "user": user, css: selectedCSS});
+	} else{
+		fetch(api + '/jogos?id=' + jogoId).then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			var a = response.json();
+			return a;
+		})
+		.then(jogo => {
+			res.render('editar-jogo', {jogo: jogo, header: setHeader(user), navclass: {"adm": "active"}, "user": user, css: selectedCSS});
+		})
+	}
+})
+
+app.listen(port, function () {
+	console.log(`Server listening on port ${port}`);
+})
+
+function setHeader(user) {
+    let header;
+	switch(user["perfil"]){
+		case "simples":
+			header = "headers/default-header.ejs";
+			break;
+		case "premium":
+			header = "headers/premium-header.ejs";
+			break;
+		case "admin":
+			header = "headers/adm-header.ejs";
+			break;
+		default:
+			header = "headers/unlogged-header.ejs";
+	}
+	return header;
+}
